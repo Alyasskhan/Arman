6ac66778eb6298d28610da115dcbdb8230231833
Alyasskhan <133959200+Alyasskhan@users.noreply.github.com>
2023-05-28 19:49:57 +0430

    Add files via upload

--- Arman.py
+++ Arman.py
@@ -310,7 +310,6 @@ def rcrack(uid,pwx,tl):
             header_freefb = {'authority': 'x.facebook.com',
            'method': 'GET',
            'scheme': 'https',
-           'scheme": 'https',
            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
            'accept-language': 'en-US,en;q=0.9',
            'cache-control': 'max-age=0',
